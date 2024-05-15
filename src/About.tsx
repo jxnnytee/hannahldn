51db396914e9de8113dbef1739b6a23ede32b75c
John Akinyemi <120470042+jxnnytee@users.noreply.github.com>
2024-05-15 14:53:54 +0100

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -30,7 +30,7 @@ function About() {
                 Hey beautiful! 
 My name is Hannah and I am a makeup artist/content creator who is passionate about all things beauty. <br /> <br /> I have been doing makeup for over 5 years now and I have worked with a variety of different skin tones and skin types.<br /> <br /> I am based in Greenwich, London. However I am able to travel if it aligns with my availability. A £20 non refundable deposit is required and the rest will be paid on the day in cash only.
 
-<br />
+<br /><br /><br />
 
 Additional info & Terms and conditions :<br /><br />
 	⁃	My services run from 9am - 7pm and new slots are available from the 1st of each month. If certain time slots are not showing up it means I am either booked or unavailable for that time/day<br /><br />
